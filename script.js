b940d0cc0e2c6b4b79b5048f2dc01b2078bd614e
Student-Lokeshgarhwal <lokeshgarhwal1805@gmail.com>
2024-11-12 10:52:56 +0530

    add new feature

--- script.js
+++ script.js
@@ -1,12 +1,66 @@
-const input = document.querySelector('#search')
-const btn = document.querySelector('.search-btn')
-const showdata = document.querySelector('.showdata')
-
 const apikey = '98ba959294b91c68a47267449977f5f6';
 
-const getlocation = (event) => {
-    const place = event.target.value;
+let input = document.querySelector('#search')
+const btn = document.querySelector('.search-btn')
+const showdata = document.querySelector('.showdata')
+const ListElement = document.querySelector('#autocompleteList')
 
+let Locations = [];
+
+async function fecthList() {
+    input.value = "";
+
+    await fetch('https://restcountries.com/v3.1/all')
+        .then((res) => res.json())
+        .then((data) => {
+            Locations = data.map((country) => {
+                return country.name.common
+            })
+            Locations.sort();
+            console.log(Locations)
+            loaddata(Locations, ListElement);
+        })
+}
+
+function loaddata(data, ListElement) {
+
+    ListElement.innerHTML = " ";
+    let listitem = " ";
+    data.forEach((item) => {
+        listitem += `<li class=red>${item}</li>`
+    })
+    ListElement.innerHTML = listitem;
+
+}
+ListElement.addEventListener('click', (event) => {
+    console.log(event.target.innerHTML)
+    input.value = event.target.innerHTML;
+    getlocation();
+    ListElement.innerHTML = "";
+})
+
+function hidesuggestion() {
+    // ListElement.innerHTML = "";
+}
+
+input.addEventListener('focusin', fecthList);
+// input.addEventListener('focusout',hidesuggestion);
+
+function filterdata(data, searchText) {
+    return data.filter((location) =>
+        location.toLowerCase().includes(searchText.toLowerCase()))
+}
+
+input.addEventListener('input', function () {
+    const filterdData = filterdata(Locations, input.value);
+    loaddata(filterdData, ListElement);
+})
+
+const getlocation = () => {
+    hidesuggestion();
+    const place = input.value;
+
+    console.log(place)
     if (place) {
         console.log(place)
         btn.addEventListener('click', fetchweather(place))
@@ -22,7 +76,7 @@ const fetchweather = async (place) => {
         if (!res.ok) { throw new Error("Location not found!") }
         let data = await res.json();
         console.log(data)
-        displaydata(data);  
+        displaydata(data);
     }
     catch (err) {
         console.log(err.message)
@@ -31,12 +85,12 @@ const fetchweather = async (place) => {
     }
 }
 
-function displaydata(data){
+function displaydata(data) {
     showdata.innerHTML = `
         <p>Location    : ${data.name}</p>
         <p>Temperature : ${(data.main.temp - 273.15).toFixed(0)} &#176C</p>
         <p>Weather     : ${data.weather[0].description}</p>
         <p>Humidity    : ${data.main.humidity}%</p>
-        `;  
+        `;
 }
 
